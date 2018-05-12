73ec0682f0e294b6de932cfe2b6be2249f499bf7
ArranGravestock <arran@gravestock.net>
2018-05-12 17:42:41 +0100

    -add path time calculation, -add strict mode

--- src/core/main.js
+++ src/core/main.js
@@ -1,51 +1,56 @@
+"use strict"
+
 window.onload = function() {
-    initialise();
+    initialise()
 }
 
 
 
-world_width = 24;
-world_height = 24;
+const world_width = 24
+const world_height = 24
 
-// tile_width = window.innerWidth / world_width;
-// tile_height = window.innerHeight / world_height;
+const tile_width = 32
+const tile_height = 32
 
-tile_width = 32;
-tile_height = 32
+// tile_width = window.innerWidth / world_width
+// tile_height = window.innerHeight / world_height
 
-world = [[]]
-current_path = []
-path_start = [world_height, world_width]
-path_end = []
 
-player_position = {}
-enemy_position = {}
 
-score = 0;
+let world = [[]]
+let current_path = []
+let path_start = [world_height, world_width]
+let path_end = []
 
-search_time = 0;
+let player_position = {}
+let enemy_position = {}
 
-enemy_interval = setInterval(update_enemy, 450)
+let score = 0
+let search_time = 0
+
+const enemy_interval = setInterval(update_enemy, 450)
+
+let canvas, ctx = false
 
 
 function initialise() {
-    canvas = document.getElementById('canvas');
+    canvas = document.getElementById('canvas')
     ctx = canvas.getContext('2d')
 
     if (ctx.getContext) {
         // canvas unsupported
     } else {
-        ctx.canvas.width  = window.innerWidth;
-        ctx.canvas.height = window.innerHeight;
+        ctx.canvas.width  = window.innerWidth
+        ctx.canvas.height = window.innerHeight
         
-        createWorld();
+        createWorld()
     } 
 }
 
 function Node(x, y, value) {
-    this.x = x;
-    this.y = y;
-    this.value = value;
+    this.x = x
+    this.y = y
+    this.value = value
 }
 
 function createWorld() {
@@ -53,13 +58,13 @@ function createWorld() {
         world[x] = []
 
         for (var y = 0; y < world_height; y++) {
-            world[x][y] = new Node(x, y, 0);
+            world[x][y] = new Node(x, y, 0)
         }
     }
     
-    generateMaze();
+    generateMaze()
     
-    draw();
+    draw()
 }
 
 
@@ -68,8 +73,8 @@ function generateMaze() {
     
     if (world.length == 9) {
 
-        world[0][0].player = true;
-        world[8][8].enemy = true;
+        world[0][0].player = true
+        world[8][8].enemy = true
 
         player_position = {x: 0,y: 0}
         enemy_position = {x: 8, y: 8}
@@ -80,7 +85,7 @@ function generateMaze() {
         world[1][3].value = 1
 
         world[1][4].coin = true
-        world[1][4].coin = true;
+        world[1][4].coin = true
 
         world[1][5].value = 1
         world[1][6].value = 0
@@ -134,8 +139,8 @@ function generateMaze() {
         world[7][6].value = 1
         world[7][7].value = 1
     } else {
-        generateRandomWalls(world_width, world_height);
-        generateCoin(4);
+        generateRandomWalls(world_width, world_height)
+        generateCoin(4)
     }
 }
 
@@ -143,7 +148,7 @@ function generateRandomWalls(width, height) {
     for (var x = 0; x < width; x++) {
         for (var y = 0; y < height; y++) {
             if (Math.random() > 0.7) {
-                world[x][y].value = 1;
+                world[x][y].value = 1
             }
         }
     }
@@ -151,54 +156,54 @@ function generateRandomWalls(width, height) {
     for(var x = 0; x < width; x++) {
         for (var y = 0; y < height; y++) {
             if (world[x][y].value == 1) {
-                var neighbours = getNeighbours(world[x][y], 1);
+                var neighbours = getNeighbours(world[x][y], 1)
                 
                 if (neighbours.length == 4) {
-                    world[x][y].value = 0;
+                    world[x][y].value = 0
                 }
             } 
 			else if (world[x][y].value == 0) {
                 var neighbours = getNeighbours(world[x][y], 1)
 				if (neighbours.length == 4) {
-					world[x+1][y].value = 1;
+					world[x+1][y].value = 1
 				}
 			}
         }
     }
 
-    player_set = false;
+    let player_set = false
     for (var x = 0; x < width; x++) {
         if (player_set) {
-            break;
+            break
         }
         for (var y = 0; y < height; y++) {
             if (world[x][y].value == 0) {
-                world[x][y].player = true;
-                player_set = true;
-                break;
+                world[x][y].player = true
+                player_set = true
+                break
             }
         }
     }
 
-    enemy_set = false;
+    let enemy_set = false
     for (var x = width-1; x > 0; x--) {
         if (enemy_set) {
-            break;
+            break
         }
         for (var y = width-1; y > 0; y--) {
             if (world[x][y].value == 0) {
-                world[x][y].enemy = true;
-                enemy_set = true;
-                break;
+                world[x][y].enemy = true
+                enemy_set = true
+                break
             }
         }
     }
 
     function getNeighbours(node, max) {
         var neighbours = []
-        var x = node.x;
-        var y = node.y;
-        var value = node.value;
+        var x = node.x
+        var y = node.y
+        var value = node.value
 
         if (y-1 >= 0 && world[x][y-1].value < max) {
             neighbours.push(world[x][y-1])
@@ -213,34 +218,29 @@ function generateRandomWalls(width, height) {
             neighbours.push(world[x+1][y])
         }
 		
-		//diagonals
-
-            
         return neighbours
     }
 }
 
 function draw() {
 
-
-
-    ctx.fillStyle = '#ccc';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = '#ccc'
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
 
     for (var x = 0; x < world_width; x++) {
         for (var y = 0; y < world_height; y++) {
 
-            ctx.beginPath();
+            ctx.beginPath()
             ctx.rect(x * tile_width, y * tile_height, tile_width, tile_height)
             
             if (world[x][y].value == 1) {
                 ctx.fillStyle = '#666'
             } else if (world[x][y].player == true) {
-                world[x][y].player = false;
+                world[x][y].player = false
                 player_position = {x: x,y: y}
                 ctx.fillStyle = 'green'
             } else if (world[x][y].enemy == true) {
-                world[x][y].enemy = false;
+                world[x][y].enemy = false
                 enemy_position = {x: x,y: y}
                 ctx.fillStyle = 'red'
             } else if (world[x][y].coin == true) {
@@ -249,43 +249,53 @@ function draw() {
                 ctx.fillStyle = 'white'
             }
 
-            ctx.fill();
-            
+            ctx.fill()
         }
     }
 
     ctx.fillStyle = 'white'
     ctx.font = "20px Arial"
-    ctx.fillText(score, world_width * tile_width + 10, 20);
-
+    ctx.fillText(score, world_width * tile_width + 10, 20)
 }
 
 function reset_game() {
-    ctx.beginPath();
+    alert(`Game over! You scored ${score} points`)
+    ctx.beginPath()
     ctx.clearRect(0, 0, canvas.width, canvas.height)
-    score = 0;
-    createWorld();
+    score = 0
+    createWorld()
 }
 
 function update_score(points) {
     score += points
-    redraw_score(score);
+    redraw_score(score)
 }
 
 function redraw_score(score) {
-    ctx.clearRect(world_width * tile_width, 0, 100,100)
+    ctx.clearRect(world_width * tile_width, 0, 100, 20)
     ctx.fillStyle = 'white'
     ctx.font = "20px Arial"
-    ctx.fillText(score, world_width * tile_width + 10, 20);
+    ctx.fillText(score, world_width * tile_width + 10, 20)
+}
+
+function update_time(time) {
+    search_time = time
+    redraw_time(search_time)
+}
+
+function redraw_time(time) {
+    ctx.clearRect(world_width * tile_width, 20, 300, 25)
+    ctx.fillStyle = 'white'
+    ctx.font = "20px Arial"
+    ctx.fillText(`Path calculated in (${time})ms`, world_width * tile_width + 10, 40)
 }
 
 function update_player(direction) {
 
-    var move_to = {x: player_position.x + direction.x, y: player_position.y + direction.y}
+    const move_to = {x: player_position.x + direction.x, y: player_position.y + direction.y}
 
     if (enemy_position.x == move_to.x && enemy_position.y == move_to.y || enemy_position.x == player_position.x && enemy_position.y == player_position.y) {
-        alert(`Game over! You scored ${score} points`);
-        reset_game();
+        reset_game()
     } else {
 
         if (!(world[move_to.x] == undefined || world[move_to.x][move_to.y] == undefined)) {
@@ -293,70 +303,68 @@ function update_player(direction) {
             if ( world[move_to.x][move_to.y].value != 1 ) {
 
                 if (world[move_to.x][move_to.y].coin == true) {
-                    world[move_to.x][move_to.y].coin = false;
-                    world[move_to.x][move_to.y].value = 0;
-                   
-                    console.log(score);
+                    world[move_to.x][move_to.y].coin = false
+                    world[move_to.x][move_to.y].value = 0
 
                     update_score(100)
-                    generateCoin(1);
+                    generateCoin(1)
                 }
     
-                ctx.beginPath();
+                ctx.beginPath()
                 ctx.clearRect(player_position.x * tile_width, player_position.y * tile_height, tile_width, tile_height)
                 ctx.fillStyle = 'white'
-                ctx.rect(player_position.x * tile_width, player_position.y * tile_height, tile_width, tile_height);
+                ctx.rect(player_position.x * tile_width, player_position.y * tile_height, tile_width, tile_height)
                 ctx.fill()
     
                 player_position.x = move_to.x
                 player_position.y = move_to.y
     
-                ctx.beginPath();
+                ctx.beginPath()
                 ctx.clearRect(player_position.x * tile_width, player_position.y * tile_height, tile_width, tile_height)
                 ctx.fillStyle = 'green'
-                ctx.rect(player_position.x * tile_width, player_position.y * tile_height, tile_width, tile_height);
+                ctx.rect(player_position.x * tile_width, player_position.y * tile_height, tile_width, tile_height)
                 ctx.fill()
     
             } else {
-                console.log("hit object");
+                console.log("hit object")
             }
         } else {
-            console.log("out of bounds");
+            console.log("out of bounds")
         }
     }
 
 }
 
 function generateCoin(number_to_draw) {
-    var i = 0;
-    var coin_generated = false;
-    for (var x = 0; x < world_width; x++) {
+    let i = 0
+    let coin_generated = false
+    for (let x = 0; x < world_width; x++) {
         if (coin_generated && i == number_to_draw) {
-            break;
+            break
         }
-        for (var y = 0; y < world_height; y++) {
+        for (let y = 0; y < world_height; y++) {
             if (Math.random() > 0.99) {
                 if (world[x][y].value == 0) {
-                    world[x][y].coin = true;
-                    world[x][y].value = 0.5;
-                    coin_generated = true;
-                    i++;
-                    break;
+                    world[x][y].coin = true
+                    world[x][y].value = 0.5
+                    coin_generated = true
+                    i++
+                    break
                 }
             }
         }
     }
-    redrawCoins();
+    redrawCoins()
 }
 
 function redrawCoins() {
-    for (var x = 0; x < world_width; x++) {
-        for (var y = 0; y < world_height; y++) {
+    for (let x = 0; x < world_width; x++) {
+        for (let y = 0; y < world_height; y++) {
             if (world[x][y].coin == true) {
-                ctx.beginPath();
+                ctx.beginPath()
                 ctx.rect(x * tile_width, y * tile_height, tile_width, tile_height)
                 ctx.fillStyle = 'yellow'
-                ctx.fill();
+                ctx.fill()
             } 
         }
     }
@@ -364,17 +372,16 @@ function redrawCoins() {
 
 function update_enemy() {
 
-    var moves = calculateNextMove(world, enemy_position, player_position)
-    var move_to = moves[1]
+    const moves = calculateNextMove(world, enemy_position, player_position)
+    const move_to = moves[1]
 
     //quick fix to reset game where unblockable path exists
     if (!move_to) {
-        reset_game();
+        reset_game()
     }
     
     if (enemy_position.x == move_to.x && enemy_position.y == move_to.y || enemy_position.x == player_position.x && enemy_position.y == player_position.y) {
-        alert("game over! you died!");
-        reset_game();
+        reset_game()
     } else {
 
         //clear the previous square the enemy was on
@@ -382,15 +389,16 @@ function update_enemy() {
 
         //if enemy steps over a coin destroy it :)
         if (world[enemy_position.x][enemy_position.y].coin == true) {
-            world[enemy_position.x][enemy_position.y].coin = false;
+            world[enemy_position.x][enemy_position.y].coin = false
             world[enemy_position.x][enemy_position.y].value = 0.5
-            generateCoin(1);
+            update_score(-100)
+            generateCoin(1)
         }
 
-        ctx.beginPath();
+        ctx.beginPath()
         ctx.clearRect(enemy_position.x * tile_width, enemy_position.y * tile_height, tile_width, tile_height)
         ctx.fillStyle = 'white'
-        ctx.rect(enemy_position.x * tile_width, enemy_position.y * tile_height, tile_width, tile_height);
+        ctx.rect(enemy_position.x * tile_width, enemy_position.y * tile_height, tile_width, tile_height)
         ctx.fill()
 
         //update the new position of the enemy
@@ -400,34 +408,34 @@ function update_enemy() {
         //update the world item
         world[enemy_position.x][enemy_position.y].enemy = true
 
-        ctx.beginPath();
+        ctx.beginPath()
         ctx.clearRect(enemy_position.x * tile_width, enemy_position.y * tile_height, tile_width, tile_height)
         ctx.fillStyle = 'red'
-        ctx.rect(enemy_position.x * tile_width, enemy_position.y * tile_height, tile_width, tile_height);
+        ctx.rect(enemy_position.x * tile_width, enemy_position.y * tile_height, tile_width, tile_height)
         ctx.fill()
     }
 }
 
 
 function calculateNextMove(world, path_start, path_end) {
-    var world_width = world[0].length;
-    var world_height = world.length;
-    var world_size = world_width * world_height;
+    var world_width = world[0].length
+    var world_height = world.length
+    var world_size = world_width * world_height
 
-    var node;
+    var node
 
-    var walkable = 0;
+    var walkable = 0
 
     function ManhattanDistance(start, end) {
-        return (Math.abs(start.x - end.x) + Math.abs(start.y - end.y));
+        return (Math.abs(start.x - end.x) + Math.abs(start.y - end.y))
     }
     
     function findPath (start, end, grid) {
         var start_x = start.x
-        var start_y = start.y;
-        var end_x = end.x;
+        var start_y = start.y
+        var end_x = end.x
         var end_y = end.y
-        var time_start = Date.now();
+        var time_start = Date.now()
 
         var closed_set = []
         var open_set = []
@@ -435,8 +443,8 @@ function calculateNextMove(world, path_start, path_end) {
         var end_node = new Node(end_x, end_y, 0)
         
         var start_node = new Node(start_x, start_y, 0)
-        start_node.g = 0;
-        start_node.f = 0;
+        start_node.g = 0
+        start_node.f = 0
 
         open_set.push(start_node)
 
@@ -450,8 +458,8 @@ function calculateNextMove(world, path_start, path_end) {
             //remove the lowest f from the openset
             for (var i = 0; i < open_set.length; i++) {
                 if (node == open_set[i]) {
-                    open_set.splice(i, 1);
-                    break;
+                    open_set.splice(i, 1)
+                    break
                 }
             }
 
@@ -462,61 +470,60 @@ function calculateNextMove(world, path_start, path_end) {
             if (node == world[end_x][end_y]) {
                 var path = [{x: node.x, y: node.y}]
                 while (node.parent) {
-                    node = node.parent;
+                    node = node.parent
                     path.push({x: node.x, y: node.y})
                 }
-                var time_end = Date.now();
-                search_time = time_end - time_start;
-                console.log(search_time);
-                return path.reverse();
+                var time_end = Date.now()
+                update_time(time_end - time_start)
+                return path.reverse()
             }
 
             //get the neighbours for the node
-            neighbours = getNeighbours(node)
+            let neighbours = getNeighbours(node)
 
             //for every neighbour
             for (var i = 0; i < neighbours.length; i++) {
-                neighbour = neighbours[i]
+                let neighbour = neighbours[i]
 
                 if (closed_set.includes(neighbour)) {
-                    continue;
+                    continue
                 }
 
                 //g = distance from starting node
                 //h = distance from end node
                 //f = g + h
 
-                x = neighbour.x;
-                y = neighbour.y;
+                let x = neighbour.x
+                let y = neighbour.y
 
                 //could be wrong
-                g = node.g + ((x - node.x === 0 || y - node.y === 0) ? 1 : Math.SQRT2)
+                let g = node.g + ManhattanDistance(neighbour, end_node)
 
                 if (!open_set.includes(neighbour) || g < neighbour.g) {
                     
-                    neighbour.g = g;
+                    neighbour.g = g
                     neighbour.h = ManhattanDistance(neighbour, end_node)
-                    neighbour.f = neighbour.g + neighbour.h;
-                    neighbour.parent = node;
+                    neighbour.f = neighbour.g + neighbour.h
+                    neighbour.parent = node
 
                     if (!open_set.includes(neighbour)) {
-                        open_set.push(neighbour);
+                        open_set.push(neighbour)
                     } else {
-                        open_set[i] = neighbour;
+                        open_set[i] = neighbour
                     }
                     
                 }
             }
             
         }
-        return [];
+        return []
     }
 
     function getNeighbours(node) {
         var neighbours = []
-        var x = node.x;
-        var y = node.y;
-        var value = node.value;
+        var x = node.x
+        var y = node.y
+        var value = node.value
 
         if (y-1 >= 0 && world[x][y-1].value < 1) {
             neighbours.push(world[x][y-1])
@@ -549,23 +556,23 @@ var config =
         "right": 68
     }
 }
-binds = config.player_binds;
+const binds = config.player_binds
 
 onkeydown =  (e) => {
-    key = e.keyCode;
+    let key = e.keyCode
 
     switch(key) {
         case binds.right: 
-            update_player({x:1, y:0});
-            break;
+            update_player({x:1, y:0})
+            break
         case binds.left:
-            update_player({x:-1, y:0});
-            break;
+            update_player({x:-1, y:0})
+            break
         case binds.up:
-            update_player({x:0, y:-1});
-            break;
+            update_player({x:0, y:-1})
+            break
         case binds.down:
-            update_player({x:0, y:1});
-            break;
+            update_player({x:0, y:1})
+            break
     }
 }
